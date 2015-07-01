f514ff06819d62fc66adff9a68a919c5823ce6ba
eligeske <eligeske@gmail.com>
2015-07-01 15:40:57 -0400

    version 1.0.0.2

--- examples/1_fakesite/flythru/assets/flythru.js
+++ examples/1_fakesite/flythru/assets/flythru.js
@@ -1,15 +1,15 @@
-var flythru = (function(){	
-	
+var flythru = (function(){
+
     var testpanel = $('#test_panel');
     var iframe = $('#speciman');
     var resetBtn = $('#reset');
-    var cw;    
-    var tests = [];   
+    var cw;
+    var tests = [];
     var loaded = false;
     var cargo = { win: null };
-    
+
     var stopped = false;
-    
+
     var config = {
         viewport: {
             h: 800, w: 900
@@ -17,7 +17,7 @@ var flythru = (function(){
         zoom: 1,
         pace: 1000
     }
-    
+
     var runSettings = function(){
         iframe.css({
             "height":config.viewport.h+"px",
@@ -27,24 +27,24 @@ var flythru = (function(){
         if(config.zoom < 1){
            iframe.css({
             "transform": "scale("+config.zoom+")"
-            }); 
+            });
         }
         var width = (($('#right_cont').width() - (iframe.width()*config.zoom)) / 2);
         var height = (($('#right_cont').height() - (iframe.height()*config.zoom)) / 2);
         $('#browser_window').css({
             'padding-left': (width > 0)?width:0,
-            'padding-top': (height > 0)?height:0,  
+            'padding-top': (height > 0)?height:0,
         });
     }
     $(window).resize(function(){
-       runSettings(); 
+       runSettings();
     });
     var startBtn = $('#run_test').click(function(){
-        timeline.start();        
+        timeline.start();
     });
     startBtn.attr('disabled',true);
     setTimeout(function(){
-        startBtn.removeAttr('disabled',true);        
+        startBtn.removeAttr('disabled',true);
     },1);
     resetBtn.click(function(){
         timeline.reset();
@@ -54,7 +54,7 @@ var flythru = (function(){
         var out = {
             start: function(){
                 stopped = false;
-                resetBtn.attr('disabled',true);                
+                resetBtn.attr('disabled',true);
                 var nextTest = function(){
                     if(stopped) return;
                     if(tests[i]){
@@ -66,13 +66,13 @@ var flythru = (function(){
                             i++;
                             nextTest(i);
                         });
-                        tests[i].takeoff();    
+                        tests[i].takeoff();
                     }else{
                         out.stop();
                         startBtn.text("Landed!").attr('disabled',true);
-                    }               
+                    }
                 };
-                nextTest(); 
+                nextTest();
             },
             stop: function(){
                 stopped = true;
@@ -81,26 +81,32 @@ var flythru = (function(){
             },
             reset: function(){
                 out.stop();
-                cargo.win.location.reload();
+                window.location.reload();
             }
         }
         return out;
     })();
-    
+
     var setWindow = function(){
-        
+
         cargo.win = (iframe[0].contentWindow)?iframe[0].contentWindow:false;
-        cargo['toolbox'] : {
+        cargo['toolbox'] = {
             angular: {
-                applyValue: (){
-                    
+								// sets value to element and also calls apply to update model
+                applyValue: function(ele,val){
+                    if(!cargo.win.angular) { throw "Angular is not defined"; return; }
+										var attr = $(ele).attr('ng-model');
+										if(attr && cargo.win.angular && cargo.win.angular.element(ele).scope){
+		                    cargo.win.angular.element(ele).scope()[attr] = val;
+		                    cargo.win.angular.element(ele).scope().$apply();
+		                }
                 }
             }
         }
-        // bind type in  
+        // bind type in
         /* if(targetJQuery){
             //flythru.cargo.win.angular.element(flythru.cargo.html.find('button:contains("Login")')[0]).scope().$apply
-            // jquery extensions 
+            // jquery extensions
            var applyAngular = function(ele){
                 var attr = ele.attr('ng-model');
                 if(attr && cargo.win.angular && cargo.win.angular.element(ele).scope){
@@ -116,22 +122,22 @@ var flythru = (function(){
         }*/
     }
 	var Test = function (name) {
-            
+
             var self = this;
-            var dom = dom;            
+            var dom = dom;
             var steps = [];
             var on = 0;
             var waits = {};
-            
+
             self.completedCallbacks = [];
-                   
+
             this.reset = function(){
                 on = 0;
                 group.removeClass("passed").removeClass("failed");
                 rowcont.find('div').removeClass("passed").removeClass("failed");
                 self.expand(false);
-            }    
-            
+            }
+
             var group = $("<div>", { "class":"group" });
             var title = $("<span>", { "class": "title", html: name });
             var rowcont = $("<div>", { "class": "check-holder" });
@@ -139,7 +145,7 @@ var flythru = (function(){
                 (!bool)?group.removeClass('open'):group.addClass('open');
             }
             group.append(title, rowcont);
-            title.click(function(){               
+            title.click(function(){
                self.expand(!$(this).parent().find('.check-holder').is(":visible"));
             });
             $("#test_panel").append(group);
@@ -152,14 +158,14 @@ var flythru = (function(){
                 if (steps[on]) {
                     on++;
                     var f = steps[on - 1];
-                    setTimeout(f, (ms = waits[(on - 1).toString()])?ms:config.pace);                   
+                    setTimeout(f, (ms = waits[(on - 1).toString()])?ms:config.pace);
                 } else {
                     if (!group.hasClass('failed')) {
                         group.addClass('passed');
                         $.each(self.completedCallbacks, function(i,func){
-                            func();                            
+                            func();
                         });
-                    }                    
+                    }
                 }
             }
             this.flyby = function () {
@@ -169,7 +175,7 @@ var flythru = (function(){
                 var name = testName;
                 var row = $("<div>", { html: "<span>" + testName + "</span>", "class": "check" });
                 rowcont.append(row);
-                steps.push(function () {                    
+                steps.push(function () {
                     func(function () {
                         row.addClass('passed');
                         next();
@@ -191,20 +197,20 @@ var flythru = (function(){
                 var howLongMS = howLongMS;
                 var func = func;
 
-                
+
 
                 steps.push(function () {
                     var pass = null;
                     var timeElapsed = 0;
                     var interval = 1000;
-                    
+
                     var failed = function(reason){
                         pass = false; row.addClass('failed'); group.addClass('failed');
                         if(reason){
                             alert(reason);
                         }
                     }
-                    
+
                     var waitForIt = function () {
                         if(pass !== null){
                             if(pass){
@@ -219,25 +225,25 @@ var flythru = (function(){
                             func(function(){
                                 pass = true; row.addClass('passed');
                             },failed,cargo);
-                                
+
                             if (timeElapsed > howLongMS) {
                                 pass = false;
-                                
+
                             }
                             waitForIt();
                         }, 1000);
                     }
                     waitForIt();
-                });          
+                });
             };
             // overloaded ()
             this.layover = function (ms,func,howLongMS) {
                 if(arguments.length > 1){
                     layoverUntil(ms,func,howLongMS);
                 }else{
-                   waits[(steps.length).toString()] = ms; 
+                   waits[(steps.length).toString()] = ms;
                 }
-                
+
                 return self;
             };
             this.takeoff = function () {
@@ -250,33 +256,33 @@ var flythru = (function(){
             }
             this.stops = steps;
         }
-	
-    
+
+
 	var innards =  {
         destination: function(url){
-            var t = new Test("Load web page"); 
+            var t = new Test("Load web page");
             tests.push(t);
             t.addStop("Navigate to: "+url,function(pass,fail,cargo){
                 startBtn.attr('disabled',true);
                 startBtn.text('boarding..');
                 iframe.load(function(){
-                    
-                    iframe.removeClass('faded');  
+
+                    iframe.removeClass('faded');
                     setTimeout(function(){
-                        
-                        setWindow();                        
+
+                        setWindow();
                         loaded = true;
                         startBtn.text('In Flight!');
-                        //startBtn.removeAttr('disabled');   
+                        //startBtn.removeAttr('disabled');
                         pass();
-                    }, 2000);         
+                    }, 2000);
                 });
-                iframe.attr('src',url);                
+                iframe.attr('src',url);
             });
             return innards;
         },
 		addFlight: function(name){
-            var t = new Test(name); 
+            var t = new Test(name);
             tests.push(t);
 			return t;
 		},
@@ -285,11 +291,11 @@ var flythru = (function(){
         },
         config: function(options){
           $.extend(config,options);
-          runSettings(); 
+          runSettings();
         },
         cargo: cargo,
         flights: tests
-	}	
+	}
     runSettings();
     return innards;
 })();
